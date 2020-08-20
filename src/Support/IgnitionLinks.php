83ebcc63b5c51ad75afa1309ee1b333831cf3e11
Randall Wilk <wilk.randall@gmail.com>
2020-08-20 07:40:27 -0500

    Update Ignition links

--- src/Support/IgnitionLinks.php
+++ src/Support/IgnitionLinks.php
@@ -5,10 +5,10 @@ namespace Rawilk\Breadcrumbs\Support;
 class IgnitionLinks
 {
     /** @var string */
-    public const DEFINING_BREADCRUMBS = 'https://docs.randallwilk.dev/laravel-breadcrumbs/v1/';
+    public const DEFINING_BREADCRUMBS = 'https://docs.randallwilk.dev/laravel-breadcrumbs/v1/usage/defining-breadcrumbs';
 
     /** @var string */
-    public const ROUTE_BOUND_BREADCRUMBS = 'https://docs.randallwilk.dev/laravel-breadcrumbs/v1/';
+    public const ROUTE_BOUND_BREADCRUMBS = 'https://docs.randallwilk.dev/laravel-breadcrumbs/v1/advanced-usage/route-bound-breadcrumbs';
 
     /** @var string */
     public const CONFIG = 'https://docs.randallwilk.dev/laravel-breadcrumbs/v1/installation#config';
@@ -17,5 +17,5 @@ class IgnitionLinks
     public const DOCS = 'https://docs.randallwilk.dev/laravel-breadcrumbs/v1/introduction';
 
     /** @var string */
-    public const CHOOSING_A_VIEW = 'https://docs.randallwilk.dev/laravel-breadcrumbs/v1/';
+    public const CHOOSING_A_VIEW = 'https://docs.randallwilk.dev/laravel-breadcrumbs/v1/usage/basic-usage#choose-a-template';
 }
