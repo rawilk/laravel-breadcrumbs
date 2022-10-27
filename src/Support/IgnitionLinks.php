a15a9cece5a334888b6d205e0449a56432d268c9
Randall Wilk <wilk.randall@gmail.com>
2022-10-27 08:41:22 -0500

    Update docs urls

--- src/Support/IgnitionLinks.php
+++ src/Support/IgnitionLinks.php
@@ -7,17 +7,17 @@ namespace Rawilk\Breadcrumbs\Support;
 class IgnitionLinks
 {
     /** @var string */
-    public const DEFINING_BREADCRUMBS = 'https://docs.randallwilk.dev/laravel-breadcrumbs/v3/usage/defining-breadcrumbs';
+    public const DEFINING_BREADCRUMBS = 'https://randallwilk.dev/docs/laravel-breadcrumbs/v3/usage/defining-breadcrumbs';
 
     /** @var string */
-    public const ROUTE_BOUND_BREADCRUMBS = 'https://docs.randallwilk.dev/laravel-breadcrumbs/v3/advanced-usage/route-bound-breadcrumbs';
+    public const ROUTE_BOUND_BREADCRUMBS = 'https://randallwilk.dev/docs/laravel-breadcrumbs/v3/advanced-usage/route-bound-breadcrumbs';
 
     /** @var string */
-    public const CONFIG = 'https://docs.randallwilk.dev/laravel-breadcrumbs/v3/installation#config';
+    public const CONFIG = 'https://randallwilk.dev/docs/laravel-breadcrumbs/v3/installation#config';
 
     /** @var string */
-    public const DOCS = 'https://docs.randallwilk.dev/laravel-breadcrumbs/v3/introduction';
+    public const DOCS = 'https://randallwilk.dev/docs/laravel-breadcrumbs/v3/introduction';
 
     /** @var string */
-    public const CHOOSING_A_VIEW = 'https://docs.randallwilk.dev/laravel-breadcrumbs/v3/usage/basic-usage#choose-a-template';
+    public const CHOOSING_A_VIEW = 'https://randallwilk.dev/docs/laravel-breadcrumbs/v3/usage/basic-usage#choose-a-template';
 }
